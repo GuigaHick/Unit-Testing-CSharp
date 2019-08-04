62fa7c44a8d83464eb9d4eb116aebefd686a632c
Guigahick <guilherme.henriquedev@gmail.com>
2019-08-04 18:30:24 -0300

    Add unit test project and MathTests

---
+++ TestNinja/TestNinjaTests/MathTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Math = TestNinja.Fundamentals.Math;
+using System.Linq;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class MathTests
+    {
+        private Math math;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            math = new Math();
+        }
+
+        [TestMethod]
+        public void Add_WhenCalled_ReturnsTheSumOfArguments()
+        {
+            var result = math.Add(1, 2);
+
+            Assert.IsTrue(result == 3);
+        }
+
+        [TestMethod]
+        public void Max_WhenCalled_ReturnTheGreatherArgument()
+        {
+            var result = math.Max(1, 0);
+
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void Max_WhenCalledWithSameArguments_ReturnTheGreatherArgument()
+        {
+            var result = math.Max(1, 1);
+
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = math.GetOddNumbers(5);
+            int[] expectedResult = new int[] { 1, 3, 5 };
+
+            Assert.IsTrue(result.SequenceEqual(expectedResult));
+        }
+    }
+}
---
+++ TestNinja/TestNinjaTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinjaTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinjaTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d35a436-77c0-4aae-bcb9-711622d1c45b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
