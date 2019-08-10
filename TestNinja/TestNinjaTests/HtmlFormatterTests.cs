283f5731f2582d3d7fbfd0795779c94c48d03ccf
Guigahick <guilherme.henriquedev@gmail.com>
2019-08-10 20:21:25 -0300

    add a unit test method for strings

---
+++ TestNinja/TestNinjaTests/HtmlFormatterTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class HtmlFormatterTests
+    {
+        [TestMethod]
+        public void FormatAsBold_WhenCalled_ShouldEncolseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+            var result = formatter.FormatAsBold("abc");
+            var expectedResult = "<strong>abc</strong>";
+
+            Assert.IsTrue(result.Equals(expectedResult));
+        }
+    }
+}
