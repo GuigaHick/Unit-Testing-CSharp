a8a5ff26b93f2fcf5dacc1f347ae7eb28ec1dd6e
Guigahick <guilherme.henriquedev@gmail.com>
2019-08-10 22:37:35 -0300

    unit test to void methods, exceptions and events

---
+++ TestNinja/TestNinjaTests/ErrorLogTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class ErrorLogTests
+    {
+        ErrorLogger logger;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            logger = new ErrorLogger();
+        }
+        //Testing Void Methods
+        [TestMethod]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            string error = "error87";
+            logger.Log(error);
+
+            Assert.IsTrue(logger.LastError.Equals(error));
+        }
+
+        //Testing Exceptions
+        [TestMethod]
+        public void Log_InvalidError_ThrowArgumentNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => { logger.Log(null); });
+        }
+
+        //Testing Raise Events
+        [TestMethod]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.IsFalse(id == Guid.Empty);
+        }
+    }
+}
