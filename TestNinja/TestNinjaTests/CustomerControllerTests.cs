cd2db712356151d9be0ca02ac71337505f516f05
Guigahick <guilherme.henriquedev@gmail.com>
2019-08-10 21:22:44 -0300

    remove generated files and add unit test for type of objects

---
+++ TestNinja/TestNinjaTests/CustomerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class CustomerControllerTests
+    {
+        [TestMethod]
+        public void GetCustomer_IdIsZero_ReturnsNotFound()
+        {
+            var customerController = new CustomerController();
+            var result = customerController.GetCustomer(0);
+
+            Assert.IsInstanceOfType(result, typeof(NotFound));
+        }
+
+        [TestMethod]
+        public void GetCustomer_IdIsNotZero_ReturnsOK()
+        {
+            var customerController = new CustomerController();
+            var result = customerController.GetCustomer(1);
+
+            Assert.IsInstanceOfType(result, typeof(Ok));
+        }
+    }
+}
