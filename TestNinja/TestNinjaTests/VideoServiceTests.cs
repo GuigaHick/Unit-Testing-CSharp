a32e9e991c7675d38ed2f3a14d982c7dd2af6abe
Guigahick <guilherme.henriquedev@gmail.com>
2019-08-18 10:41:40 -0300

    dependency injection and unit test using moq

---
+++ TestNinja/TestNinjaTests/VideoServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TestNinja.Mocking;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class VideoServiceTests
+    {
+        IFileReader fileReader;
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            Mock<IFileReader> mockFileReader = new Mock<IFileReader>();
+            mockFileReader.Setup(fr => fr.Read("video.txt")).Returns(string.Empty);
+            fileReader = mockFileReader.Object;
+        }
+
+        [TestMethod]
+        public void ReadVideoTitle_EmptyFile_ReturnsErrorMessage()
+        {
+            VideoService videoService = new VideoService(fileReader);
+            var result = videoService.ReadVideoTitle();
+            Assert.IsTrue(result.Contains("Error"));
+        }
+    }
+}
