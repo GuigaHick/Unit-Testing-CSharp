a62cfcedaf6aa989da42dbde8f785072711c152d
Guigahick <guilherme.henriquedev@gmail.com>
2019-08-18 10:59:36 -0300

    using verify method of moq

---
+++ TestNinja/TestNinjaTests/OrderServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Mocking;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class OrderServiceTests
+    {
+        [TestMethod]
+        public void PlaceOrder_WhenCalled_StoreTheOrder()
+        {
+            Mock<IStorage> storage = new Mock<IStorage>();
+            var service = new OrderService(storage.Object);
+
+            var order = new Order();
+            service.PlaceOrder(order);
+
+            storage.Verify(s => s.Store(order));
+        }
+    }
+}
