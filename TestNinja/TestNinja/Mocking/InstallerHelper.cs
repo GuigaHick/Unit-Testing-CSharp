6727fd5ae9660221921183b155f7c9ee90518ed3
Guigahick <guilherme.henriquedev@gmail.com>
2019-09-15 11:48:29 -0300

    refactor on installer helper and tests

--- TestNinja/TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/TestNinja/Mocking/InstallerHelper.cs
@@ -5,18 +5,19 @@ namespace TestNinja.Mocking
     public class InstallerHelper
     {
         private string _setupDestinationFile;
+        private IFileDownloader fileDownloader;
+
+        public InstallerHelper(IFileDownloader fileDownloader)
+        {
+            this.fileDownloader = fileDownloader;
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
-            var client = new WebClient();
             try
             {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
-                    _setupDestinationFile);
-
+                string url = string.Format("http://example.com/{0}/{1}",customerName, installerName);
+                fileDownloader.DownloadFile(url, _setupDestinationFile);
                 return true;
             }
             catch (WebException)
---
+++ TestNinja/TestNinjaTests/InstallerHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Net;
+using TestNinja.Mocking;
+
+namespace TestNinjaTests
+{
+    [TestClass]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> mockFileDowloader;
+        private InstallerHelper installer;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockFileDowloader = new Mock<IFileDownloader>();
+            installer = new InstallerHelper(mockFileDowloader.Object);
+        }
+
+        [TestMethod]
+        public void DownloadInstaller_DownloadFails_ReturnsFalse()
+        {
+            mockFileDowloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+            Assert.IsFalse(installer.DownloadInstaller("customer", "installer"));
+        }
+
+        [TestMethod]
+        public void DownloadInstaller_DownloadCompletes_ReturnsFalse()
+        {
+            mockFileDowloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()));
+            Assert.IsTrue(installer.DownloadInstaller("customer", "installer"));
+        }
+    }
+}
